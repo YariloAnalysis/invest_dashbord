c3a07059b00577e513c1d4e1fbc56e01e56f3001
YariloAnalysis <yarilo.urzhumov@mail.ru>
2026-02-27 01:43:17 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,29 +1,5 @@
+
 import streamlit as st
-import pandas as pd
-import requests
-import os
-from dotenv import load_dotenv
-
-def get_api_url():
-    try:
-        return st.secrets["API_URL"]
-    except Exception:
-        load_dotenv()
-        return os.getenv("API_URL", "http://localhost:8000")
-
-def get_api_key():
-    try:
-        return st.secrets["API_KEY"]
-    except Exception:
-        load_dotenv()
-        return os.getenv("API_KEY", "")
-
-@st.cache_data(ttl=600)
-def select(query, params=None):
-    try:
-        api_url = get_api_url()
-        api_key = get_api_key()
-        import streamlit as st
 import pandas as pd
 import requests
 import os
