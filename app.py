7d8599eb76c130128470b45681cc5a05988ccc54
YariloAnalysis <yarilo.urzhumov@mail.ru>
2026-04-11 16:14:56 +0500

    Update app.py
    переход на мультипользовательский режим

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-
+# app.py
 import streamlit as st
 
 st.set_page_config(
     layout="wide",
     page_title="Мой портфель",
-    page_icon="📊"
+    page_icon="📊",
 )
-st.write("Добро пожаловать что вас интересует?")
+
+from auth import require_auth, logout_button
+
+require_auth()
+logout_button()
+
+st.write("Добро пожаловать! Что вас интересует?")
